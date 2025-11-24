3e3dcff6058e6e674d32bb89dbba87ff93a42779
BEHRUZBEK JUMABOYEV <jumaboyevbehruz148@gmail.com>
2025-11-24 15:15:05 +0500

    1

---
+++ yechim.py
@@ -0,0 +1,12 @@
+#1-masala
+for i in range(1, 11):
+    print(i, end=" ")
+#2-masala
+word = "Python"
+for l in word:
+    print(" -", l)
+#3-masala
+names = ["Ali", "Vali", "Hasan", "Husan"]
+for index, name in enumerate(names, start=1):
+    print(f"{index}. {name}")
+#4-masala
